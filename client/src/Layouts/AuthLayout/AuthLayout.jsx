f7221c435bda9fdef195c2c922104f601380f762
Your Name <you@example.com>
2025-07-29 10:44:14 +0600

    change the authentication layout design

--- client/src/Layouts/AuthLayout/AuthLayout.jsx
+++ client/src/Layouts/AuthLayout/AuthLayout.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router';
 
 const AuthLayout = () => {
-    return (
-        <div className='min-h-screen px-4 bg-gradient-to-r from-green-50 to-purple-50 '>
-        <div className=''>
-          <Outlet />
-        </div>
+  return (
+    <div className="min-h-screen bg-gradient-to-tr from-[#fda085] to-[#f85606]/80 flex items-center justify-center px-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-2xl p-8">
+        <Outlet />
       </div>
-    );
+    </div>
+  );
 };
 
 export default AuthLayout;
